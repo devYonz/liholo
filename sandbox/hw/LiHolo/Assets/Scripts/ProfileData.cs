dbed7985425864c4a89102dfce05576a4c2f18cb
hwray <hmcwray@gmail.com>
2017-07-24 17:50:58 -0700

    Added JSON loading and profile data classes.

---
+++ sandbox/hw/LiHolo/Assets/Scripts/ProfileData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ProfileData {
+
+    [System.Serializable]
+    public class BirthDate
+    {
+        public int month;
+        public int year;
+        public int day; 
+    }
+
+    public string firstName;
+    public string lastName;
+    public string locationName;
+    public string industryName;
+    public string headline;
+    public string summary;
+    public BirthDate birthDate;
+}
---
+++ sandbox/hw/LiHolo/Assets/Scripts/ProfileDataController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ProfileDataController : MonoBehaviour {
+
+    [HideInInspector]
+    public ProfileData profileData;
+
+    public void LoadProfileData(string fileName)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName); 
+
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            profileData = JsonUtility.FromJson<ProfileData>(jsonData);
+        }
+        else
+        {
+            Debug.LogError("Error loading profile data from JSON"); 
+        }
+    }
+}

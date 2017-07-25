a7d83b6819649de6d527bec97e63383f5e6108cb
hwray <hmcwray@gmail.com>
2017-07-25 14:32:03 -0700

    Created animated UI with mixed 2D canvas text and 3D buttons.

--- sandbox/hw/LiHolo/Assets/Scripts/ProfileDataController.cs
+++ sandbox/hw/LiHolo/Assets/Scripts/ProfileDataController.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class ProfileDataController : MonoBehaviour {
 
+    public string fileName; 
+
     [HideInInspector]
     public ProfileData profileData;
+
+    public void Start()
+    {
+        LoadProfileData(fileName); 
+    }
 
     public void LoadProfileData(string fileName)
     {
@@ -16,10 +24,33 @@ public class ProfileDataController : MonoBehaviour {
         {
             string jsonData = File.ReadAllText(filePath);
             profileData = JsonUtility.FromJson<ProfileData>(jsonData);
+
+            gameObject.GetComponentInChildren<Text>().text = profileData.firstName + " " + profileData.lastName; 
         }
         else
         {
             Debug.LogError("Error loading profile data from JSON"); 
         }
     }
+}
+
+[System.Serializable]
+public class ProfileData
+{
+
+    [System.Serializable]
+    public class BirthDate
+    {
+        public int month;
+        public int year;
+        public int day;
+    }
+
+    public string firstName;
+    public string lastName;
+    public string locationName;
+    public string industryName;
+    public string headline;
+    public string summary;
+    public BirthDate birthDate;
 }
---
+++ sandbox/hw/LiHolo/Assets/Scripts/ProfileMenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule; 
+
+public class ProfileMenuItem : MonoBehaviour, IFocusable {
+
+    private Animation anim; 
+
+	void Start()
+    {
+        anim = gameObject.GetComponent<Animation>(); 
+	}
+
+    void IFocusable.OnFocusEnter()
+    {
+        print("Enter");
+
+        anim.PlayQueued("a_Highlight");
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+        print("Exit");
+
+        anim.PlayQueued("a_UnHighlight"); 
+    }
+}
